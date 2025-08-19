1d1f00d80a2dbb31f6cee96198c9096d7744948e
Ralphwasnotfound <ralphjosephbatiancila1914@gmail.com>
2025-08-19 13:54:35 +0800

    tweaks

--- src/router/index.js
+++ src/router/index.js
@@ -1,33 +1,16 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '@/views/HomeView.vue'
 import AboutView from '@/views/AboutView.vue'
 import ProjectsView from '@/views/ProjectsView.vue'
 import ContactView from '@/views/ContactView.vue'
 
-
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
-    {
-      path: '/',
-      name: 'Home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'About',
-      component: AboutView
-    },
-    {
-      path: '/projects',
-      name: 'Projects',
-      component: ProjectsView
-    },
-    {
-      path: '/contact',
-      name: 'Contact',
-      component: ContactView
-    },
+    { path: '/', name: 'Home', component: HomeView },
+    { path: '/about', name: 'About', component: AboutView },
+    { path: '/projects', name: 'Projects', component: ProjectsView },
+    { path: '/contact', name: 'Contact', component: ContactView },
   ],
 })
 
